285db8d9b03c6e815f2ef0b52605f19baa008a90
Poonam <pneelay@source360group.com>
2022-01-03 12:08:44 -0600

    Added Auth token

--- src/config/threekitConfig.js
+++ src/config/threekitConfig.js
@@ -1,5 +1,5 @@
-const authToken =
-  process.env.REACT_APP_PUBLIC_TOKEN || 'e8d26c36-e722-41ef-917b-bab77f0cea99';
+//const authToken =
+//  process.env.REACT_APP_PUBLIC_TOKEN || 'e8d26c36-e722-41ef-917b-bab77f0cea99';
 const orgId =
   process.env.REACT_APP_ORG_ID || 'eb6756f4-8c6e-417b-8926-8ad4b1ed6895';
 
@@ -9,7 +9,8 @@ export default {
   // Staging
   // authToken: 'f026609b-7611-4a0a-aae0-8f36e3208393',
   // Local
-  authToken,
+  
+  authToken: window.location.hostname == 'msasafety' ? 'dd4c07cd-f7e0-4668-b52d-e9f54b3decb2' : 'e8d26c36-e722-41ef-917b-bab77f0cea99',
   assetId: '6b7bdb94-44eb-4713-b192-91c78c2dd967',
   orgId,
 };
